fa9fa2785420db04d2954a972f00949d25544897
Federico Olive <FedericoOlive@gmail.com>
2021-09-17 16:11:44 -0300

    Add door garage.

---
+++ Assets/Scripts/Ui/Ui_OpenGarage.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ui_OpenGarage : MonoBehaviour
+{
+    [SerializeField] private float startPos = 27;
+    [SerializeField] private float posUp = 17.5f;
+    [SerializeField] private float maxTime = 3;
+    private float onTime;
+
+    private Vector3 posInit;
+    private Vector3 posEnd;
+    void Start()
+    {
+        posInit = transform.position;
+        posEnd = transform.position;
+        posEnd.y = posUp;
+
+        if (maxTime == 0)
+            DestroyImmediate(this);
+    }
+    void Update()
+    {
+        onTime += Time.unscaledDeltaTime;
+        if (onTime < maxTime)
+        {
+            transform.position = Vector3.Lerp(posInit, posEnd, onTime / maxTime);
+        }
+        else
+        {
+            Destroy(this);
+        }
+    }
+}
