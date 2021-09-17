d85d96b535056eeea6176cd177f27a6ab6c49a10
Federico Olive <FedericoOlive@gmail.com>
2021-09-17 09:41:54 -0300

    Update Textures. Add TextMeshPro.

---
+++ Assets/Scripts/Ui_Manager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Ui_Manager : MonoBehaviour
+{
+    public List<CanvasGroup> panelsPlayer = new List<CanvasGroup>();
+    void Start()
+    {
+        
+    }
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Ui_Player.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Ui_Player : MonoBehaviour
+{
+    public GameObject player;
+    public List<Sprite> truckSprites = new List<Sprite>();
+    public Image truckImage;
+    public TextMeshProUGUI textMoney;
+
+    void Start()
+    {
+        
+    }
+    void Update()
+    {
+        
+    }
+
+    public void AddScore(int amount)
+    {
+        textMoney.text = "$" + amount;
+    }
+}
