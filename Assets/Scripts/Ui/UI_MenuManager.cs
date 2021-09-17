02ac471f48462d9f388d935ff4209911d65b2286
Federico Olive <FedericoOlive@gmail.com>
2021-09-17 10:47:31 -0300

    Add Main Menu.

---
+++ Assets/Scripts/Ui/UI_MenuManager.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+public class UI_MenuManager : MonoBehaviour
+{
+    [SerializeField] private CanvasGroup[] menues;
+    [SerializeField] private float timeTransition = 1;
+    [Space(10)]
+    [SerializeField] TextMeshProUGUI versionText = null;
+
+    void Start()
+    {
+        Time.timeScale = 1;
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+        versionText.text += Application.version;
+    }
+
+    enum Menues { Main, Play, Credits, Options, Exit }
+    private Menues menuActual = Menues.Main;
+    private float onTime;
+
+    public void OffPanel()
+    {
+        menues[(int)menuActual].blocksRaycasts = false;
+        menues[(int)menuActual].interactable = false;
+        StartCoroutine(PanelOff(timeTransition, (int)menuActual));
+    }
+    public void SwitchPanel(int otherMenu)
+    {
+        menues[(int) menuActual].blocksRaycasts = false;
+        menues[(int) menuActual].interactable = false;
+        StartCoroutine(SwitchPanel(timeTransition, otherMenu, (int) menuActual));
+    }
+    IEnumerator SwitchPanel(float maxTime, int onMenu, int offMenu)
+    {
+        CanvasGroup on = menues[onMenu];
+        CanvasGroup off = menues[offMenu];
+
+        while (onTime < maxTime)
+        {
+            onTime += Time.deltaTime;
+            float fade = onTime / maxTime;
+            on.alpha = fade;
+            off.alpha = 1 - fade;
+            yield return null;
+        }
+        on.blocksRaycasts = true;
+        on.interactable = true;
+        onTime = 0;
+
+        menuActual = (Menues)onMenu;
+    }
+    IEnumerator PanelOff(float maxTime, int offMenu)
+    {
+        CanvasGroup off = menues[offMenu];
+
+        while (onTime < maxTime)
+        {
+            onTime += Time.deltaTime;
+            float fade = onTime / maxTime;
+            off.alpha = 1 - fade;
+            yield return null;
+        }
+        onTime = 0;
+    }
+    public void ChangeScene(string scene)
+    {
+        SceneManager.LoadScene(scene);
+    }
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
---
+++ Assets/Scripts/Ui/Ui_RotateCam.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class Ui_RotateCam : MonoBehaviour
+{
+    public Transform cam;
+    public Transform center;
+    private float speed = 2;
+
+    private void Start()
+    {
+        
+    }
+    private void Update()
+    {
+        cam.transform.RotateAround(center.position, Vector3.up, speed);
+    }
+}
